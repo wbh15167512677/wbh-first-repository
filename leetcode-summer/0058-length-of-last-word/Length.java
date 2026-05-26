4252cd65c84a7f34e868a017601537bb64f2fbd7
wbh15167512677 <bohao.wang2006@gmail.com>
2026-05-25 17:31:59 -0700

    Add LeetCode problem 58 solution

---
+++ leetcode-summer/0058-length-of-last-word/Length.java
@@ -0,0 +1,17 @@
+class Length {
+    public int lengthOfLastWord(String s) {
+        int index = s.length() - 1;
+        int length = 0;
+
+        while (index >= 0 && s.charAt(index) == ' ') {
+            index--;
+        }
+
+        while (index >= 0 && s.charAt(index) != ' ') {
+            length++;
+            index--;
+        }
+
+        return length;
+    }
+}
