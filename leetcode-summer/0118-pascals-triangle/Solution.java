2bb385cd72a0915a9ba460341251b35904e62b7f
wbh15167512677 <bohao.wang2006@gmail.com>
2026-05-28 16:40:00 -0700

    Add LeetCode problem 118 solution

---
+++ leetcode-summer/0118-pascals-triangle/Solution.java
@@ -0,0 +1,26 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> generate(int numRows) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        for (int i = 0; i < numRows; i++) {
+            List<Integer> row = new ArrayList<>();
+
+            for (int j = 0; j <= i; j++) {
+                if (j == 0 || j == i) {
+                    row.add(1);
+                } else {
+                    int num = result.get(i - 1).get(j - 1) 
+                            + result.get(i - 1).get(j);
+                    row.add(num);
+                }
+            }
+
+            result.add(row);
+        }
+
+        return result;
+    }
+}
