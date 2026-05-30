2686f3ea993f5cf1b47eadb07d4c27cec1b203d5
wbh15167512677 <bohao.wang2006@gmail.com>
2026-05-30 12:27:50 -0700

    Add LeetCode problem 1 solution

---
+++ leetcode-summer/0001-two-sum/Solution.java
@@ -0,0 +1,20 @@
+import java.util.HashMap;
+import java.util.Map;
+
+class Solution {
+    public int[] twoSum(int[] nums, int target) {
+        Map<Integer, Integer> map = new HashMap<>();
+
+        for (int i = 0; i < nums.length; i++) {
+            int need = target - nums[i];
+
+            if (map.containsKey(need)) {
+                return new int[] { map.get(need), i };
+            }
+
+            map.put(nums[i], i);
+        }
+
+        return new int[] {};
+    }
+}
