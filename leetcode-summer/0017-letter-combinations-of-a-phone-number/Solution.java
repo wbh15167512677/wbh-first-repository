02ce1d345ae602c29f6625abfc6cb467c6479c8d
wbh15167512677 <bohao.wang2006@gmail.com>
2026-06-01 17:09:38 -0700

    Add LeetCode problem 17 solution

---
+++ leetcode-summer/0017-letter-combinations-of-a-phone-number/Solution.java
@@ -0,0 +1,45 @@
+import java.util.ArrayList;
+import java.util.List;
+
+class Solution {
+    public List<String> letterCombinations(String digits) {
+        List<String> result = new ArrayList<>();
+
+        if (digits == null || digits.length() == 0) {
+            return result;
+        }
+
+        String[] map = {
+            "",     // 0
+            "",     // 1
+            "abc",  // 2
+            "def",  // 3
+            "ghi",  // 4
+            "jkl",  // 5
+            "mno",  // 6
+            "pqrs", // 7
+            "tuv",  // 8
+            "wxyz"  // 9
+        };
+
+        backtrack(digits, 0, new StringBuilder(), result, map);
+
+        return result;
+    }
+
+    private void backtrack(String digits, int index, StringBuilder path,
+                           List<String> result, String[] map) {
+        if (index == digits.length()) {
+            result.add(path.toString());
+            return;
+        }
+
+        String letters = map[digits.charAt(index) - '0'];
+
+        for (int i = 0; i < letters.length(); i++) {
+            path.append(letters.charAt(i));
+            backtrack(digits, index + 1, path, result, map);
+            path.deleteCharAt(path.length() - 1);
+        }
+    }
+}
