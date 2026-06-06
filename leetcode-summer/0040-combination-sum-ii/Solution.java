576dc7e592fc6df49e537e166b1ca7dd4bac5d25
wbh15167512677 <bohao.wang2006@gmail.com>
2026-06-06 16:25:19 -0700

    Add LeetCode problem 40 solution

---
+++ leetcode-summer/0040-combination-sum-ii/Solution.java
@@ -0,0 +1,39 @@
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+class Solution {
+    public List<List<Integer>> combinationSum2(int[] candidates, int target) {
+        List<List<Integer>> result = new ArrayList<>();
+
+        Arrays.sort(candidates);
+
+        backtrack(candidates, target, 0, new ArrayList<>(), result);
+
+        return result;
+    }
+
+    private void backtrack(int[] candidates, int remain, int start,
+                           List<Integer> path, List<List<Integer>> result) {
+        if (remain == 0) {
+            result.add(new ArrayList<>(path));
+            return;
+        }
+
+        for (int i = start; i < candidates.length; i++) {
+            if (i > start && candidates[i] == candidates[i - 1]) {
+                continue;
+            }
+
+            if (candidates[i] > remain) {
+                break;
+            }
+
+            path.add(candidates[i]);
+
+            backtrack(candidates, remain - candidates[i], i + 1, path, result);
+
+            path.remove(path.size() - 1);
+        }
+    }
+}
