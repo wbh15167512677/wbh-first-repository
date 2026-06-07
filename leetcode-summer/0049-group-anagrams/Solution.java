316708c2a65236284c701607ca47a3224de5f520
wbh15167512677 <bohao.wang2006@gmail.com>
2026-06-07 15:44:20 -0700

    Add LeetCode problem 49 solution

---
+++ leetcode-summer/0049-group-anagrams/Solution.java
@@ -0,0 +1,35 @@
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+class Solution {
+    public List<List<String>> groupAnagrams(String[] strs) {
+        Map<String, List<String>> map = new HashMap<>();
+
+        for (String str : strs) {
+            int[] count = new int[26];
+
+            for (char c : str.toCharArray()) {
+                count[c - 'a']++;
+            }
+
+            StringBuilder keyBuilder = new StringBuilder();
+
+            for (int i = 0; i < 26; i++) {
+                keyBuilder.append('#');
+                keyBuilder.append(count[i]);
+            }
+
+            String key = keyBuilder.toString();
+
+            if (!map.containsKey(key)) {
+                map.put(key, new ArrayList<>());
+            }
+
+            map.get(key).add(str);
+        }
+
+        return new ArrayList<>(map.values());
+    }
+}
