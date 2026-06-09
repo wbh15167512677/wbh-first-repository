bf7bc8d1b97bd481a2a8d31891811ba0de03f691
wbh15167512677 <bohao.wang2006@gmail.com>
2026-06-08 18:02:41 -0700

    Add LeetCode problem 53 solution

--- leetcode-summer/0053-maximum-subarray/Solution.java
+++ leetcode-summer/0053-maximum-subarray/Solution.java
@@ -1,42 +1,13 @@
-import java.util.ArrayList;
-import java.util.List;
-
 class Solution {
-    public List<Integer> spiralOrder(int[][] matrix) {
-        List<Integer> result = new ArrayList<>();
+    public int maxSubArray(int[] nums) {
+        int currentSum = nums[0];
+        int maxSum = nums[0];
 
-        int top = 0;
-        int bottom = matrix.length - 1;
-        int left = 0;
-        int right = matrix[0].length - 1;
-
-        while (top <= bottom && left <= right) {
-            
-            for (int j = left; j <= right; j++) {
-                result.add(matrix[top][j]);
-            }
-            top++;
-
-            for (int i = top; i <= bottom; i++) {
-                result.add(matrix[i][right]);
-            }
-            right--;
-
-            if (top <= bottom) {
-                for (int j = right; j >= left; j--) {
-                    result.add(matrix[bottom][j]);
-                }
-                bottom--;
-            }
-
-            if (left <= right) {
-                for (int i = bottom; i >= top; i--) {
-                    result.add(matrix[i][left]);
-                }
-                left++;
-            }
+        for (int i = 1; i < nums.length; i++) {
+            currentSum = Math.max(nums[i], currentSum + nums[i]);
+            maxSum = Math.max(maxSum, currentSum);
         }
 
-        return result;
+        return maxSum;
     }
 }
