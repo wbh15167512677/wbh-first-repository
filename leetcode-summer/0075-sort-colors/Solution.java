0e655b3117ea8459b8c51a309163632b4296fa4d
wbh15167512677 <bohao.wang2006@gmail.com>
2026-06-12 15:56:39 -0700

    Add LeetCode problem 75 solution

---
+++ leetcode-summer/0075-sort-colors/Solution.java
@@ -0,0 +1,26 @@
+class Solution {
+    public void sortColors(int[] nums) {
+        int left = 0;
+        int right = nums.length - 1;
+        int i = 0;
+
+        while (i <= right) {
+            if (nums[i] == 0) {
+                swap(nums, i, left);
+                i++;
+                left++;
+            } else if (nums[i] == 2) {
+                swap(nums, i, right);
+                right--;
+            } else {
+                i++;
+            }
+        }
+    }
+
+    private void swap(int[] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+}
