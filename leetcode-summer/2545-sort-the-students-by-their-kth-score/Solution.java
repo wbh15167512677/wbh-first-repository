3ec87ca5672eb2be5f78ef39e3becf8d80e93d02
wbh15167512677 <bohao.wang2006@gmail.com>
2026-06-27 15:06:35 -0700

    Add LeetCode problem 2545 solution

---
+++ leetcode-summer/2545-sort-the-students-by-their-kth-score/Solution.java
@@ -0,0 +1,9 @@
+import java.util.Arrays;
+
+class Solution {
+    public int[][] sortTheStudents(int[][] score, int k) {
+        Arrays.sort(score, (a, b) -> b[k] - a[k]);
+
+        return score;
+    }
+}
